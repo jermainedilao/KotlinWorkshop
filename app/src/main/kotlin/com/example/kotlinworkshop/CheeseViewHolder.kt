9b3255fd0edb60ab8f4546d05294d283c736b7a5
Jermaine Dilao <jermaine@sym.ph>
2017-11-17 22:18:06 +0800

    Run listview

---
+++ app/src/main/kotlin/com/example/kotlinworkshop/CheeseViewHolder.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinworkshop
+
+import android.view.View
+import android.widget.TextView
+
+
+class CheeseViewHolder(view: View) {
+    val textView: TextView = view.findViewById(R.id.textView)
+}
--- app/src/main/kotlin/com/example/kotlinworkshop/MainActivityKt.kt
+++ app/src/main/kotlin/com/example/kotlinworkshop/MainActivityKt.kt
@@ -1,12 +1,23 @@
 package com.example.kotlinworkshop
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivityKt : AppCompatActivity() {
+
+    lateinit var adapter: CheeseAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = CheeseAdapter(this, CheeseKt.LIST)
+        listView.adapter = adapter
+    }
+
+    fun onButtonClick(view: View) {
+
     }
 }
