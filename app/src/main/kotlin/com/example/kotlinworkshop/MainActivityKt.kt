d5e74b70a6bb728eedcab7ba8d42c948ef99b724
Alistair Cerio <alistair.cerio@gmail.com>
2017-11-17 19:05:20 +0800

    initial commit

---
+++ app/src/main/kotlin/com/example/kotlinworkshop/MainActivityKt.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinworkshop
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityKt : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
